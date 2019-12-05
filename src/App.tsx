47264fece18d888b50cb4e6e95295e16fd260a35
Liriconst <liriconst@gmail.com>
2019-12-05 13:11:14 +0300

    Work with App header

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,29 @@ const App: React.FC = () => {
       <Router history={appHistory}>
         <div className={styles.appHeader}>
           <div className={styles.appHeaderStart}>
-            <div className={styles.appHeaderStartAddress}>
-              <div className={styles.appHeaderStartDoubleText}><span>г. Хадыженск, ул. Первомайская,</span></div>
-              <div className={styles.appHeaderStartDoubleText}>д. 103 "А", 2 этаж, кабинет 6</div>
+            <div className={styles.appHeaderStartPart1}>
+              <div className={styles.appHeaderStartAddress}>
+                <div className={styles.appHeaderStartDoubleText}><span>г. Хадыженск, ул. Первомайская,</span></div>
+                <div className={styles.appHeaderStartDoubleText}>д. 103 "А", 2 этаж, кабинет №6</div>
+              </div>
+              <span className={styles.appHeaderStartAddressSeparator}/>
+              <div className={styles.appHeaderStartSchedule}>
+                <div className={styles.appHeaderStartDoubleText}><span>Пн, Вт</span></div>
+                <div className={styles.appHeaderStartDoubleText}>Ср, Пт</div>
+              </div>
+              <span className={styles.appHeaderStartScheduleSeparator}/>
+              <div className={styles.appHeaderStartNumber}>9</div>
+              <div className={styles.appHeaderStartTextOO}>ОО</div>
+              <div className={styles.appHeaderStartDash}>-</div>
+              <div className={styles.appHeaderStartNumber}>17</div>
+              <div className={styles.appHeaderStartTextOO}>ОО</div>
             </div>
-            <span className={styles.appHeaderStartAddressSeparator}/>
-            <div className={styles.appHeaderStartSchedule}>
-              <div className={styles.appHeaderStartDoubleText}><span>Пн, Вт</span></div>
-              <div className={styles.appHeaderStartDoubleText}>Ср, Пт</div>
+            <div className={styles.appHeaderStartPart2}>
+              <div className={styles.appHeaderStartNet}><img src="/static/svg/7-vk-White.svg" alt={""}/></div>
+              <div className={styles.appHeaderStartNet}><img src="/static/svg/8-instagram-White.svg" alt={""}/></div>
+              <div className={styles.appHeaderStartButton}><span>Запись online</span></div>
+              <span className={styles.appHeaderStartPhone}>+7(918)177-24-17</span>
             </div>
-            <span className={styles.appHeaderStartScheduleSeparator}/>
-            <div className={styles.appHeaderStartText9}>9</div>
-            <div className={styles.appHeaderStartTextOO}>ОО</div>
             {/* <div><Link to="/">Home</Link></div>*/}
             {/*<div><Link to="/menu">Menu</Link></div>*/}
             {/*<div>*/}
